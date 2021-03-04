011ecc9ffacd968fa98c8b9955a438660fb7a353
IsaikinSergei <akinfest007@gmail.com>
2021-03-05 01:48:56 +0400

    Added CourseVC,TableViewCell and WebVC

---
+++ Networking/CoursesViewController.swift
@@ -0,0 +1,52 @@
+//
+//  CoursesViewController.swift
+//  Networking
+//
+//  Created by Sergei Isaikin on 05.03.2021.
+//
+
+import UIKit
+
+class CoursesViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension CoursesViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! TableViewCell
+        
+        return cell
+    }
+    
+    
+}
+
+extension CoursesViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+}
---
+++ Networking/TableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  TableViewCell.swift
+//  Networking
+//
+//  Created by Sergei Isaikin on 05.03.2021.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var courseImage: UIImageView!
+    @IBOutlet weak var courseNameLabel: UILabel!
+    @IBOutlet weak var numberOfLessons: UILabel!
+    @IBOutlet weak var numberOfTests: UILabel!
+    
+    
+    
+}
